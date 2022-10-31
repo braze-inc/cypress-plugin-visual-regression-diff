4dd18fcf938dfa1c728c650898a7b10b56eec3ad
Ian Leue <ian@braze.com>
2022-10-31 17:34:45 -0400

    fix: Package correctly when releasing to git

--- release.config.js
+++ release.config.js
@@ -1,16 +1,35 @@
 module.exports = {
-    "branches": ["main"],
-    "plugins": [
-        ["@semantic-release/commit-analyzer", {
-            "releaseRules": [
-                {"type": "chore", "scope": "deps", "release": "patch"},
-                {"type": "docs", "release": "patch"}
-            ]
-        }],
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-        "@semantic-release/changelog",
-        "@semantic-release/git"
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "chore", scope: "deps", release: "patch" },
+          { type: "docs", release: "patch" },
+        ],
+      },
     ],
-    "preset": "angular"
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "yarn prepack",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "dist/**/*.{js,mjs,map}",
+          "LICENSE",
+          "Readme.md",
+          "package.json",
+        ],
+      },
+    ],
+  ],
+  preset: "angular",
 };
